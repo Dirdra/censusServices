9fa524458424a4ab975c42e7b1ce2ef83f7ca125
Dirdra <dirdra@online.de>
2021-12-29 15:12:50 +0100

    :fire: reducing to event-listening
    removing of all other stuff that is not nesessary for event listening
    (may come back later)

--- eventServer/src/main/java/de/dirdra/censusEventServer/AppConfig.java
+++ eventServer/src/main/java/de/dirdra/censusEventServer/AppConfig.java
@@ -1,29 +1,18 @@
 package de.dirdra.censusEventServer;
 
 import javax.annotation.PreDestroy;
-import javax.jms.ConnectionFactory;
-import javax.jms.Destination;
 
-import org.apache.activemq.command.ActiveMQTopic;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jms.annotation.EnableJms;
-import org.springframework.jms.listener.DefaultMessageListenerContainer;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.client.WebSocketClient;
 import org.springframework.web.socket.client.WebSocketConnectionManager;
 import org.springframework.web.socket.client.standard.StandardWebSocketClient;
 
-import de.dirdra.censusEventServer.websocket.DefaultMessageHandler;
-import de.dirdra.censusEventServer.websocket.TopicListener;
-import de.dirdra.censusEventServer.websocket.WebSocketMessageHandler;
-
 @Configuration
-@EnableJms
 public class AppConfig {
 	
 	private static final Logger LOG = LoggerFactory.getLogger(AppConfig.class);
@@ -31,18 +20,17 @@ public class AppConfig {
 	@Value("${dbg.census.push.url}")
 	private String censusURL = "wss://push.planetside2.com/streaming?environment=ps2&service-id=s:";
 	
-	@Autowired
 	private WebSocketConnectionManager webSocketConnectionManager;
 	
 	@Bean
-	@Autowired
-	public WebSocketConnectionManager connectionManager(@Value("${websocket.connectionManager.autostart:true") String autostart) {
+	public WebSocketConnectionManager connectionManager(@Value("${websocket.connectionManager.autostart:true") String autostart, WebSocketClient webSocketClient, WebSocketHandler webSocketHandler) {
 		LOG.info("censusurl > {}", censusURL);
-		final WebSocketConnectionManager manager = new WebSocketConnectionManager(websocketClient(), webSocketHandler(), censusURL);
+		final WebSocketConnectionManager manager = new WebSocketConnectionManager(webSocketClient, webSocketHandler, censusURL);
 		manager.setAutoStartup(Boolean.valueOf(autostart));
 		manager.start();
 		
 		LOG.debug("connectionManager created: {}", manager);
+		this.webSocketConnectionManager = manager;
 		return manager;
 	}
 
@@ -62,28 +50,12 @@ public class AppConfig {
 		return client;
 	}
 	
-	@Bean
-	public WebSocketMessageHandler createWebSocketMessageHandler() {
-		return new DefaultMessageHandler();
-	}
-	
-	@Bean
-	public TopicListener createTopicListener() {
-		return new TopicListener();
-	}
-	
-	@Bean
-	public DefaultMessageListenerContainer createMessageListenerContainer(ConnectionFactory connectionFactory) {
-		final DefaultMessageListenerContainer container = new DefaultMessageListenerContainer();
-		container.setConnectionFactory(connectionFactory);
-		final Destination destination = new ActiveMQTopic("myTopic");
-		container.setDestination(destination);
-		container.setMessageListener(createTopicListener());
-		return container;
-	}
-	
 	@PreDestroy
 	public void onShutdown() {
-		webSocketConnectionManager.stop();
+	    if (webSocketConnectionManager != null) {
+	        webSocketConnectionManager.stop();
+	    } else {
+	        LOG.warn("webSocketConnectionManager is null");
+	    }
 	}
 }
